d92e8633b1e094e126037260fb72d7e82fc03da0
Jérôme Tamarelle <jerome@tamarelle.net>
2012-04-16 23:15:20 +0200

    Move bootstrap for tests, composer required.

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Use composer autoloader depending of the installation context.
+ */
+if (file_exists($file = __DIR__.'/../../../.composer/autoload.php')
+ || file_exists($file = __DIR__.'/../vendor/.composer/autoload.php')) {
+    $loader = require $file;
+    $loader->add('Buzy\\Tests', __DIR__);
+} else {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
