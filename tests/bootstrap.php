d6e6825880b1ec208a3eaca054e28ada41fed325
Jérôme Tamarelle <jerome@tamarelle.net>
2012-05-04 23:43:23 +0200

    Update composer dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,5 +10,5 @@ if (file_exists($file = __DIR__.'/../../../.composer/autoload.php')
 } else {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
+        'php composer.phar install --dev'.PHP_EOL);
 }
